270f6ce258e1ce300658a617400ab2ffe77d864c
Daniel Baulé <dsbaule@gmail.com>
2018-11-24 18:21:37 -0200

    Improving CFG string

--- CFG.py
+++ CFG.py
@@ -8,19 +8,20 @@ class CFG:
 
     def __str__(self):
         string =  \
-            'Terminals: ' + str(self.terminals) + '\n' \
-            'Non-Terminals: ' + str(self.nonTerminals) + '\n' \
-            'Productions:\n' + \
-            "\t{:<15} {:<100}\n".format('Non-Terminal','Production')
+            'Terminals: ' + str(self.terminals) + '\n\n' \
+            'Non-Terminals: ' + str(self.nonTerminals) + '\n\n' \
+            'Productions:\n\n' + \
+            "\t{:<15} {:<100}\n\n".format('Non-Terminal','Production')
         for key, value in self.productions.items():
-            string += "\t{:<15}".format(key)
+            string += "\t{:<15} ".format(key)
             for symbol in value[0]:
                 string += symbol + ' '
             string += '\n'
             if len(value) > 1:
                 for prod in value[1:]:
-                    string += "\t{:<15}".format(' ')
+                    string += "\t{:<15} ".format(' ')
                     for symbol in prod:
                         string += symbol + ' '
                     string += '\n'
+        string += '\n'
         return string
