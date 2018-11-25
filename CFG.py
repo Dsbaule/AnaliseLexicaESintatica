012a8bd69bcdadd87dd92456925fe4509958473a
Daniel Baulé <dsbaule@gmail.com>
2018-11-25 18:34:03 -0200

    First()

--- CFG.py
+++ CFG.py
@@ -5,6 +5,15 @@ class CFG:
         self.terminals = terminals
         self.nonTerminals = nonTerminals
         self.productions = productions
+        self.first = dict()
+        self.first['&'] = {'&'}
+        for symbol in self.terminals:
+            self.first[symbol] = {symbol}
+        for symbol in self.nonTerminals:
+            self.first[symbol] = set()
+        self.follow = dict()
+        self.table = dict()
+        self.generateTable()
 
     def __str__(self):
         string =  \
@@ -23,5 +32,35 @@ class CFG:
                     for symbol in prod:
                         string += symbol + ' '
                     string += '\n'
+        string += '\nFirsts:'
+        for nonTerminal in self.nonTerminals:
+            string += "\t{:<15} {:<80}\n".format(nonTerminal, str(self.first[nonTerminal]))
         string += '\n'
         return string
+
+    def generateTable(self):
+        self.generateFirst()
+        #self.generateFollow()
+
+    def generateFirst(self):
+        oldFirst = dict()
+
+        while(True):
+            done = True
+            oldFirst.clear()
+            for nonTerminal in self.nonTerminals:
+                oldFirst[nonTerminal] = self.first[nonTerminal].copy()
+            for nonTerminal, productions in self.productions.items():
+                for production in productions:
+                    for symbol in production:
+                        first = self.first[symbol]
+                        # print("\t{:<15} {:<30} {:<100}\n".format(nonTerminal, symbol, str(first)))
+                        self.first[nonTerminal].update(first)
+                        if '&' not in first:
+                            break
+            for nonTerminal in self.nonTerminals:
+                if oldFirst[nonTerminal] != self.first[nonTerminal]:
+                    done = False
+                    break
+            if done:
+                break
